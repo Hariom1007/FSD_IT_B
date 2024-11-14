1589bc356651935f97ed43508224d02b2c850efc
Hari om upadhyay <harry523610@gmail.com>
2024-11-14 12:02:10 +0530

    kk

---
+++ JS/react-cdn.js
@@ -0,0 +1,5 @@
+const parent=document.getElementById("root");
+const element=React.createElement("h1",{},"This is new Heading");
+const root=ReactDOM.createRoot(parent);
+      root.render(element);
+const h1=React.createElement("h1",{},"Shopping Cart");
